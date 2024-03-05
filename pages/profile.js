ed7c39843d87e749657c52dfe881a41e5cb6cd0f
Daus Carmichael <daviscarmichael@chadlike.lan>
2024-03-04 18:43:31 -0800

    fixed mismatched rendering by removing easter egg :((((

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,15 @@
 import Header from "../components/Header";
 import Profile from "../components/Profile";
-// replacing this: import { Navigate } from 'react-router-dom';
-// with this:
-import Perhaps from "../components/Perhaps";
-import { isLoggedIn } from "../functions/LoginBool";
+import ProtectedRoute from "./_ProtectedRoute";
+
 
 export default function ProfilePage() {
     return (
-        <div>
-            
-            {isLoggedIn()?<><Header /><Profile /></>:<Perhaps />}
-        </div>
+        <ProtectedRoute>
+            <div>
+                <Header />
+                <Profile />
+            </div>
+        </ProtectedRoute>
     );
 }
