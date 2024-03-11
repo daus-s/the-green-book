9d02808ee0ae137d18a946a2b93c906d46cc3770
daus-s <dauscarmichael@gmail.com>
2024-03-10 18:08:23 -0700

    editted ProfileNav.js created profile menu selector on

--- components/ProfileNav.js
+++ components/ProfileNav.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import { useAuth } from "./providers/AuthContext";
+import { useMobile } from "./providers/MobileContext";
 
 export default function ProfileNav() {
-    const {meta} = useAuth();
+    const [profileMenu, setProfileMenuVisible] = useState(false);
+    const {isMobile} = useMobile();
+    const {meta, logout} = useAuth();
     //{mapping(user)?mapping(user):"user.png"}
-    return (<div className="profile-nav"><img src={meta.pfp} onClick={(e)=>window.location.href="/profile"}/></div>)
+    const onClick = (e) => {
+        if (!isMobile) {
+            window.location.href="/profile";
+            return;
+        } 
+        else if (isMobile) {
+            setProfileMenuVisible(true);
+        }
+    }
+    return (
+        <div className="profile-nav">
+            <img src={meta.pfp} onClick={onClick} style={isMobile?{'cursor': 'default'}:{}}/>
+            {profileMenu?(
+                <div className="profile-menu-mobile">
+                    <div className="profile-link" href="/profile">Profile</div>
+                    <div className="sign-out" onClick={logout}>Sign-out</div>
+                </div>)
+            :
+                <></>
+            }
+        </div>)
 }
