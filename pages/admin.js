fcddefaf9c26c20bc307649f6bd19fcee89463ed
daus-s <dauscarmichael@gmail.com>
2024-04-01 17:31:01 -0700

    added protected route to admin page should upgrade to require user to be admin

--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
 import Admin from "../components/Admin";
+import ProtectedRoute from "./_ProtectedRoute";
 
 export default function AdminPage() {
-    return <Admin/>
+    useEffect(()=> {
+
+    }, [])
+    return (
+        <ProtectedRoute>
+            <Admin/>
+        </ProtectedRoute>
+    );
 }
