69f511bd3f9d821681c3df3f9d5f99a042069017
daus-s <dauscarmichael@gmail.com>
2024-04-09 11:33:18 -0700

    i guess the master page will be hosted like this on a pagewhere you can create a bet for a grooup or a head on matchup

---
+++ pages/masters.js
@@ -0,0 +1,9 @@
+import MastersPlaceBetForm from "../components/MastersBetPlaceForm";
+
+export default function MastersPage() {
+    return (
+        <div className="masters page">
+            <MastersPlaceBetForm />
+        </div>
+    );
+}
