33579d28658cc19394d003ec6a71d28f9e783caa
daus-s <dauscarmichael@gmail.com>
2024-10-27 17:24:59 -0700

    using a new compoennt for the calendar to save some time

--- components/timing/TimeSelector.js
+++ components/timing/TimeSelector.js
@@ -1,7 +1,7 @@
 import { isTomorrow } from "date-fns";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { generateDates } from "../../functions/Calend";
+import { useState } from "react";
+import Calendar from "react-calendar";
 
 export default function DateTimePicker({ time, setTime }) {
     if (!(new Date(time) && typeof setTime === "function")) {
@@ -19,69 +19,10 @@ export default function DateTimePicker({ time, setTime }) {
 }
 
 function DTModal({ time, setTime }) {
-    const [year, setYear] = useState(time.getFullYear());
-    const [month, setMonth] = useState(time.getMonth());
-    const [day, setDay] = useState(time.getDate());
-    const [hour, setHour] = useState(time.getHours());
-
-    const nextMonth = (e) => {
-        e.preventDefault();
-
-        let mTemp = month;
-        mTemp++;
-
-        if (mTemp >= 12) {
-            mTemp %= 12;
-            setYear((y) => y++);
-        }
-
-        setMonth(mTemp);
-    };
-    const prevMonth = (e) => {
-        e.preventDefault();
-
-        let mTemp = month;
-        mTemp--;
-
-        if (mTemp >= 12) {
-            mTemp %= 12;
-            setYear((y) => y++);
-        }
-
-        setMonth(mTemp);
-    };
-
-    // useEffect(() => {}, [day, hour, month, year]);
-    const dates = generateDates(month, year);
-
     return (
         <div className="date-time-modal" style={{ position: "absolute" }}>
-            <MonthSelector month={month} increment={nextMonth} decrement={prevMonth} />
-            {dates ? <Calendar dates={dates} /> : <></>}
+            <Calendar value={time} onChange={setTime} />
             <TimeSelect />
-        </div>
-    );
-}
-
-function Calendar({ dates }) {
-    return (
-        <div className="cldnr">
-            <div className="hd"></div>
-            {dates?.map((date) => {
-                return <DaySelector date={date} />;
-            })}
-        </div>
-    );
-}
-
-function DaySelector({ date }) {
-    console.log(date);
-    const weekConstant = ["a", "b", "c", "d", "e", "f"][date.week];
-    const dateConstant = ["th", "fr", "sa", "su", "mo", "tu", "we"][date.dow];
-
-    return (
-        <div className={`cb ${weekConstant} ${dateConstant}`} key={`${date.basetime}`}>
-            {date.display}
         </div>
     );
 }
@@ -102,18 +43,6 @@ function FieldIcon({ time, setIsOpen }) {
             <div className="img-hover-wrapper" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} style={hover ? { backgroundColor: "var(--soft-highlight)" } : {}}>
                 <Image src="/calendar.png" alt={"Pick date and time to finalize bet."} height={24} width={24} onClick={() => setIsOpen(true)} />
             </div>
-        </div>
-    );
-}
-
-function MonthSelector({ month, increment, decrement }) {
-    const months = ["Decmeber", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
-
-    return (
-        <div>
-            <button onClick={decrement}>{"<"}</button>
-            {months[month]}
-            <button onClick={increment}>{">"}</button>
         </div>
     );
 }
