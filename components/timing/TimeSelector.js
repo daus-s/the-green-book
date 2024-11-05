0dc585bfb936a1c47fcba74a65b5425956de9266
daus-s <dauscarmichael@gmail.com>
2024-11-04 21:34:41 -0800

    fuck u e.stopPropagation();

--- components/timing/TimeSelector.js
+++ components/timing/TimeSelector.js
@@ -1,38 +1,49 @@
 import { isTomorrow } from "date-fns";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import { useState } from "react";
 import Calendar from "react-calendar";
 
 export default function DateTimePicker({ time, setTime }) {
     if (!(new Date(time) && typeof setTime === "function")) {
         throw new Error("invalid state management and parameter");
     }
-
     const [isOpen, setIsOpen] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (isOpen && ref.current && !ref.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener("click", handleClickOutside);
+        return () => document.removeEventListener("click", handleClickOutside);
+    }, [isOpen]);
 
     return (
         <div style={{ position: "relative" }}>
-            {isOpen ? <DTModal time={time} setTime={setTime} /> : <></>}
+            {isOpen ? <DTModal time={time} setTime={setTime} ref={ref} /> : <></>}
             <FieldIcon time={time} setIsOpen={setIsOpen} />
         </div>
     );
 }
 
-function DTModal({ time, setTime }) {
+const DTModal = React.forwardRef(function DTModal({ time, setTime }, ref) {
+    const format = (_locale, date) => ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"][date.getUTCDay()];
     return (
-        <div className="date-time-modal" style={{ position: "absolute" }}>
-            <Calendar value={time} onChange={setTime} />
+        <div className="date-time-modal" style={{ position: "absolute" }} ref={ref}>
+            <Calendar value={time} onChange={setTime} formatShortWeekday={format} />
             <TimeSelect />
         </div>
     );
-}
+});
 
 function TimeSelect() {
     return;
 }
 
 function FieldIcon({ time, setIsOpen }) {
-    const [hover, setHover] = useState(false);
     const h = time.getHours();
     const m = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
     return (
@@ -40,8 +51,17 @@ function FieldIcon({ time, setIsOpen }) {
             {isTomorrow(time)
                 ? "Tomorrow, " + (h % 12 === 0 ? 12 : h % 12) + ":" + m + (h > 12 ? "PM" : "AM")
                 : time.getMonth() + 1 + "/" + time.getDate() + "/" + time.getFullYear() + " " + (h % 12 === 0 ? 12 : h % 12) + ":" + m + (h > 12 ? "PM" : "AM")}
-            <div className="img-hover-wrapper" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} style={hover ? { backgroundColor: "var(--soft-highlight)" } : {}}>
-                <Image src="/calendar.png" alt={"Pick date and time to finalize bet."} height={24} width={24} onClick={() => setIsOpen(true)} />
+            <div className="img-hover-wrapper">
+                <Image
+                    src="/calendar.png"
+                    alt={"Pick date and time to finalize bet."}
+                    height={24}
+                    width={24}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setIsOpen(true);
+                    }}
+                />
             </div>
         </div>
     );
